b2d90fe05be592adc3ec58fceaa0e2c5647e328d
Rajesh Jondhale <jondhaleraj2151@gmail.com>
2023-04-03 17:23:42 +0530

    day 24 completed

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,6 +1,10 @@
 import 'catalog.dart';
 
 class CartModel {
+  static final cartModel = CartModel._internal();
+
+  CartModel._internal();
+  factory CartModel() => cartModel;
   //catalog field
   late CatalogModel _catalog;
 
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/models/cart.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
@@ -28,12 +29,13 @@ class _cartTotal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = CartModel();
     return SizedBox(
       height: 100,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          "\$999".text.xl4.color(context.accentColor).make(),
+          "\$${_cart.totalPrice}".text.xl4.color(context.accentColor).make(),
           30.widthBox,
           ElevatedButton(
             onPressed: () {
@@ -62,17 +64,18 @@ class _cartList extends StatefulWidget {
 }
 
 class __cartListState extends State<_cartList> {
+  final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 5,
+      itemCount: _cart.items?.length,
       itemBuilder: (context, index) => ListTile(
         leading: Icon(Icons.done),
         trailing: IconButton(
           icon: Icon(Icons.remove_circle_outline),
           onPressed: () {},
         ),
-        title: "Item1".text.make(),
+        title: _cart.items[index].name.text.make(),
       ),
     );
   }
